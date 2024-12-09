bf6bb4930bd0fba76196e0aea71ec802f9eb4c47
jamesBoder <james.macean08@gmail.com>
2024-12-09 18:29:15 -0500

    7th commit

--- simple_shopping_cart/js/application.js
+++ simple_shopping_cart/js/application.js
@@ -6,9 +6,10 @@ $(document).ready(function () {
     let cost = unitPrice * quantity;
     $(ele).children('.cost').html("$" + `${cost.toFixed(2)}`);
   });
+  total();
 });
 
-$(document).ready(function () {
+var total = function (){
   const consts = document.querySelectorAll('.cost');
   let totalCost = 0;
 
@@ -18,4 +19,29 @@ $(document).ready(function () {
   })
 
   document.getElementById('cartTotal').textContent = `${totalCost.toFixed(2)}`
+};
+
+
+var sum = function (acc, x) {return acc + x;};
+
+var updateCart = function () {
+  var costColumn = [];
+
+  $('tbody tr').each(function(i, ele) {
+    let cost = total();
+    costColumn.push(cost);
+  });
+
+  var cartTotal = costColumn.reduce(sum);
+  document.getElementById('cartTotal').html(cartTotal);
+};
+
+$(document).ready(function () {
+
+  $('.btn.remove').on('click', function (event) {
+    $(this).closest('tr').remove();
+    updateCart();
+  });
 });
+
+
